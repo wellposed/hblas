faf4a815b87229211cb0d2e843d52bb042cffd98
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2013-12-28 01:04:09 -0500

    augmented test example to have an actual computation.

--- Test.hs
+++ Test.hs
@@ -11,5 +11,7 @@ main = do
   -- Just test that the symbol resolves
   openblas_set_num_threads_ffi 1
   v  :: IOVector Double <- M.replicate 10 1.0
+  res <- unsafeWith v (\ptr-> cblas_ddot_ffi 10 (castPtr ptr) 1 (castPtr ptr) 1) 
+  putStrLn $ show res 
  
   putStrLn "it works?"
