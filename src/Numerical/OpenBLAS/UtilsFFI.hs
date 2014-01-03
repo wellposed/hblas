21980b419595375346dc313569d2856443769118
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-01-03 18:02:55 -0500

    adding openblas build caching plus cleanup on ffi utils

--- src/Numerical/OpenBLAS/UtilsFFI.hs
+++ src/Numerical/OpenBLAS/UtilsFFI.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Trustworthy #-}
+{- VERY TRUST WORTHY :) -}
 module Numerical.OpenBLAS.UtilsFFI where
 
 
@@ -13,7 +14,7 @@ withRWStorable:: (Storable a, PrimMonad m)=> a -> (Ptr a -> m b) -> m a
 withRWStorable val fun = do 
     valVect <- M.replicate 1 val 
     _ <- unsafeWithPrim valVect fun 
-    M.read valVect 0 
+    M.unsafeRead valVect 0 
 {-# INLINE withRWStorable #-}    
 
 
@@ -24,25 +25,6 @@ withRStorable val fun = do
 {-# INLINE withRStorable #-} 
 
 withForeignPtrPrim :: PrimMonad m => ForeignPtr a -> (Ptr a -> m b) -> m b
--- ^This is a way to look at the pointer living inside a
--- foreign object.  This function takes a function which is
--- applied to that pointer. The resulting 'IO' action is then
--- executed. The foreign object is kept alive at least during
--- the whole action, even if it is not used directly
--- inside. Note that it is not safe to return the pointer from
--- the action and use it after the action completes. All uses
--- of the pointer should be inside the
--- 'withForeignPtr' bracket.  The reason for
--- this unsafeness is the same as for
--- 'unsafeForeignPtrToPtr' below: the finalizer
--- may run earlier than expected, because the compiler can only
--- track usage of the 'ForeignPtr' object, not
--- a 'Ptr' object made from it.
---
--- This function is normally used for marshalling data to
--- or from the object pointed to by the
--- 'ForeignPtr', using the operations from the
--- 'Storable' class.
 withForeignPtrPrim  fo act
   = do r <- act (unsafeForeignPtrToPtr fo)
        touchForeignPtrPrim fo
