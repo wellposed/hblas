238c9e58b36e5bbabc335953ba228ae811c81bb0
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-01-09 00:02:57 -0500

    woops, didn’t update names in test.hs !

--- testing/Test.hs
+++ testing/Test.hs
@@ -9,9 +9,9 @@ import Foreign.Ptr
 main :: IO ()
 main = do
   -- Just test that the symbol resolves
-  openblas_set_num_threads_ffi 7  
+  openblas_set_num_threads_unsafe 7  
   v  :: IOVector Double <- M.replicate 10 1.0
-  res <- unsafeWith v (\ptr-> cblas_ddot_ffi 10 (castPtr ptr) 1 (castPtr ptr) 1) 
+  res <- unsafeWith v (\ptr-> cblas_ddot_unsafe 10 (castPtr ptr) 1 (castPtr ptr) 1) 
   putStrLn $ show res 
   putStrLn "it works!"
   
