9e379e44111f2754bf405ea50c4c35d3f0227006
Dorus Leliveld <dorusleliveld@gmail.com>
2016-12-18 14:01:43 -0600

    Changed from hspec-discover to manual imports

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,1 +1,16 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+import qualified HBLAS.BLAS.Level1Spec
+import qualified HBLAS.BLAS.Level2Spec
+import qualified HBLAS.BLAS.Level3Spec
+import qualified HBLAS.MatrixTypesSpec
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "HBLAS.BLAS.Level1" HBLAS.BLAS.Level1Spec.spec
+  describe "HBLAS.BLAS.Level2" HBLAS.BLAS.Level2Spec.spec
+  describe "HBLAS.BLAS.Level3" HBLAS.BLAS.Level3Spec.spec
+  describe "HBLAS.MatrixTypes" HBLAS.MatrixTypesSpec.spec
