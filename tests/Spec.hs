ebb29eb561119be00a54f265db39f335eb2c779a
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2017-04-26 16:16:26 -0400

    disable level1 routine implementations and tests for now, renable in new major version release after fixed

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,4 +1,4 @@
-import qualified HBLAS.BLAS.Level1Spec
+--import qualified HBLAS.BLAS.Level1Spec
 import qualified HBLAS.BLAS.Level2Spec
 import qualified HBLAS.BLAS.Level3Spec
 import qualified HBLAS.MatrixTypesSpec
@@ -10,7 +10,7 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "HBLAS.BLAS.Level1" HBLAS.BLAS.Level1Spec.spec
+  --describe "HBLAS.BLAS.Level1" HBLAS.BLAS.Level1Spec.spec
   describe "HBLAS.BLAS.Level2" HBLAS.BLAS.Level2Spec.spec
   describe "HBLAS.BLAS.Level3" HBLAS.BLAS.Level3Spec.spec
   describe "HBLAS.MatrixTypes" HBLAS.MatrixTypesSpec.spec
